5b39e9d78b89c5c96469eea7185ab47d7f110bb6
AgostinaArevaloJaube <agosarevalo@gmail.com>
2022-02-02 21:16:11 -0300

    Add dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Input } from "@nextui-org/react";
+import styled from "styled-components";
+
+const StyledInput = styled(Input.Password)`
+  background-color: pink;
+`;
 
 export default function Home() {
   return (
@@ -12,12 +18,15 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <StyledInput
+          clearable
+          bordered
+          labelPlaceholder="Name"
+          initialValue="Soy Julian"
+        />
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,12 +67,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
